1bac36d189dd55c9f8f9f80ad0866d8dff7bedce
Jenkins <jenkins@ip-172-31-23-180.ec2.internal>
2014-12-30 01:59:28 +0000

    Compiled Version

---
+++ build/scripts/strategies/auto-bet.js
@@ -0,0 +1,1 @@
+define(["strategies/auto-bet/widget","strategies/auto-bet/logic","strategies/auto-bet/validation","strategies/auto-bet/initial-state"],function(e,t,n,r){var i=e;return i.logic=t,i.validate=n,i.initialState=r,i.isWidget=!0,i});
